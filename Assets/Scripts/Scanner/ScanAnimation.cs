90ed653d7dc212b1ca1384f6729f881f957105f3
MEI YIFAN <s10268922@connect.np.edu.sg>
2026-01-30 00:58:35 +0800

    lazer vfx setup

--- Assets/Scripts/Scanner/ScanAnimation.cs
+++ Assets/Scripts/Scanner/ScanAnimation.cs
@@ -1,48 +1,77 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class ScanAnimation : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioSource;
+
+    [Header("Animation")]
     public string trigger = "ScanStart";
+
+    [Header("Input")]
     public InputActionReference ActivateScanner;
-    public bool ScannerReady;
+
+    [Header("Audio")]
+    public AudioClip scanSound;
+    public float audioDelay = 2f;
+
+    [Header("Cooldown")]
+    public bool ScannerReady = true;
+    public float cooldownTime = 15f;
+
     private float time;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
         ActivateScanner.action.started += ScannerStartTrigger;
-        ScannerReady = true;
         time = 0;
     }
+
     void Update()
     {
         if (!ScannerReady)
         {
             time += Time.deltaTime;
-        }
-        if (time > 15.0f)
-        {
-            ScannerReady = true;
-            time = 0;
+            if (time > cooldownTime)
+            {
+                ScannerReady = true;
+                time = 0;
+            }
         }
     }
+
     void ScannerStartTrigger(InputAction.CallbackContext context)
     {
-        if (ScannerReady)
-        {
-            ScannerReady = false;
-            StartSpinning();
-        }
-        else
+        if (!ScannerReady)
         {
             Debug.Log("Scanner Anim cooldown");
+            return;
         }
+
+        ScannerReady = false;
+        StartSpinning();
+        StartCoroutine(PlayScanSoundDelayed());
     }
+
     public void StartSpinning()
     {
         animator.SetTrigger(trigger);
     }
 
+    IEnumerator PlayScanSoundDelayed()
+    {
+        yield return new WaitForSeconds(audioDelay);
+
+        if (scanSound != null)
+        {
+            audioSource.PlayOneShot(scanSound);
+        }
+    }
 }
